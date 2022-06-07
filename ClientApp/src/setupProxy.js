16aa31e3164ab0f77bb765b5eed0fb2fa48b02c8
Richard Osborne <romosborne@gmail.com>
2022-06-07 20:24:39 +0100

    No WS

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -5,13 +5,12 @@ const target = env.ASPNETCORE_URLS
   ? env.ASPNETCORE_URLS.split(";")[0]
   : "http://localhost:62109";
 
-const context = ["/weatherforecast", "/room", "/ws"];
+const context = ["/weatherforecast", "/room"];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false,
-    ws: true,
     headers: {
       Connection: "Keep-Alive",
     },
